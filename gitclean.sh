af7966ccffee951ec4555cd3e52f8efe6d4db0e6
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2023-08-05 14:10:56 -0700

    Add project cache cleaning tool

---
+++ gitclean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+DIR_CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Clean working directory: ${DIR_CWD}"
+
+cd ${DIR_CWD}
+rm -rf CMakeCache.txt
+rm -rf cmake_install.cmake
+rm -rf Makefile
+rm -rf build
+rm -rf CMakeFiles
+
+
