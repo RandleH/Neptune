62b87f1636d77b93d4db1d6c4add65615a4e8b9d
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2023-08-06 23:57:25 -0700

    Add initial project legacy

---
+++ main.c
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  RandleH
+  * @brief   Main program entrance
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 RandleH.
+  * All rights reserved.
+  *
+  * This software component is licensed by RandleH under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+
+int main( int argc, char const *argv[]){
+    while(1){
+        
+    }
+    return 0;
+}
