8cf5f5ae946365ef9f86faa0fec050925c8fc4bc
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2023-10-12 00:14:21 -0700

    Milestone: CMake stable

--- lib/rh_lib.cmake
+++ lib/rh_lib.cmake
@@ -3,22 +3,25 @@ cmake_minimum_required(VERSION 3.14)
 include(FetchContent)
 
 MACRO( CHECK_LIB_INTEGRITY lib_dir result)
-    message( "- Check library integrity @ ${lib_dir}..." )
-    execute_process(
-        COMMAND
-            git -C ${lib_dir} fsck
-        RESULT_VARIABLE
-            RES
-    )
-    message( "- Returned value ${RES}" )
-    if( (NOT ${RES} EQUAL 0) OR (NOT EXISTS "${lib_dir}/.git"))
-        set( ${result} FALSE)
+    if( EXISTS "${lib_dir}" )
+        execute_process(
+            COMMAND
+                git -C ${lib_dir} fsck
+            RESULT_VARIABLE
+                RES
+        )
+        message( "- Program returned value ${RES}" )
+        if( (NOT EXISTS "${lib_dir}/.git") OR (NOT ${RES} EQUAL 0))
+            set( ${result} FALSE)
+        else()
+            set( ${result} TRUE)
+        endif()
     else()
-        set( ${result} TRUE)
+        set( ${result} FALSE)
     endif()
 ENDMACRO()
 
-message( "RANDLEH in LIB: ${CMAKE_C_COMPILER}")
+
 
 
 
@@ -27,20 +30,23 @@ message( "RANDLEH in LIB: ${CMAKE_C_COMPILER}")
 # Website: 
 # GitHub: https://github.com/lvgl/lvgl.git
 #########################################################################################################
+message( STATUS "Check library integrity @ ${PRJ_TOP}/lib/lvgl ..." )
 SET( LIB_DIR__LVGL  ${PRJ_TOP}/lib/lvgl)
 CHECK_LIB_INTEGRITY( ${LIB_DIR__LVGL} result)
 if( ${result} STREQUAL FALSE)
-    FetchContent_Declare(   lvgl
+    FetchContent_Declare(   lib_lvgl
                             GIT_REPOSITORY https://github.com/lvgl/lvgl.git
                             SOURCE_DIR ${LIB_DIR__LVGL})
+    FetchContent_MakeAvailable(lib_lvgl)                        
 endif()
+
 
 set( LV_CONF_BUILD_DISABLE_EXAMPLES  TRUE)
 set( DLV_CONF_BUILD_DISABLE_DEMOS  TRUE)
 set( LVGL_MISC_DEFINE       -DLV_CONF_BUILD_DISABLE_EXAMPLES=1
                             -DLV_CONF_BUILD_DISABLE_DEMOS=1
                             -DLV_CONF_PATH=${CMAKE_SOURCE_DIR}/cfg/lv_conf.h
-                            -DLV_USE_DEV_VERSION)
+                            )
 
 add_definitions( ${LVGL_MISC_DEFINE})
 add_subdirectory( ${LIB_DIR__LVGL} ${PRJ_TOP}/build/lvgl/build)
@@ -53,14 +59,10 @@ target_link_options(lvgl INTERFACE ${LINK_FLAG} ${CPU_FLAG})
 # lvgl_example
 
 
-if( ${result} STREQUAL FALSE)
-    FetchContent_MakeAvailable(lvgl)
-endif()
-
 list( APPEND INC_DIR_LIST   ${LIB_DIR__LVGL})
 list( APPEND TARGET_LIST    lvgl)
 
-
+message( STATUS "LVGL library loaded")
 
 
 #########################################################################################################
@@ -69,12 +71,14 @@ list( APPEND TARGET_LIST    lvgl)
 # GitHub: https://github.com/FreeRTOS/FreeRTOS-Kernel.git
 #########################################################################################################
 SET( LIB_DIR__FREERTOS  ${PRJ_TOP}/lib/FreeRTOS-Kernel)
+message( STATUS "Check library integrity @ ${LIB_DIR__FREERTOS} ..." )
 CHECK_LIB_INTEGRITY( ${LIB_DIR__FREERTOS} result)
 if( ${result} STREQUAL FALSE)
     FetchContent_Declare(   freertos_kernel
                             GIT_REPOSITORY https://github.com/FreeRTOS/FreeRTOS-Kernel.git
                             GIT_TAG        main #Note: Best practice to use specific git-hash or tagged version 
                             SOURCE_DIR ${LIB_DIR__FREERTOS})
+    FetchContent_MakeAvailable(freertos_kernel)
 endif()
 
 if (CMAKE_CROSSCOMPILING)
@@ -86,7 +90,6 @@ endif()
 
 set( FREERTOS_HEAP "4" CACHE STRING "" FORCE)
 
-
 add_library(freertos_config INTERFACE)
 target_include_directories(freertos_config SYSTEM INTERFACE ${PRJ_TOP}/cfg)
 target_compile_definitions(freertos_config INTERFACE projCOVERAGE_TEST=0)
@@ -97,14 +100,12 @@ target_link_options(freertos_config INTERFACE ${LINK_FLAG} ${CPU_FLAG})
 add_subdirectory( ${LIB_DIR__FREERTOS}   ${PRJ_TOP}/build/freertos/build)
 
 
-if( ${result} STREQUAL FALSE)
-    FetchContent_MakeAvailable(freertos_kernel)
-endif()
 
 list( APPEND INC_DIR_LIST           ${LIB_DIR__FREERTOS}/include
                                     ${LIB_DIR__FREERTOS}/portable/GCC/ARM_CM4F)
 list( APPEND TARGET_LIST            freertos_kernel)
 
+message( STATUS "FreeRTOS-Kernel library loaded")
 
 
 #########################################################################################################
@@ -115,11 +116,11 @@ list( APPEND TARGET_LIST            freertos_kernel)
 SET( LIB_DIR__STM32HAL  ${PRJ_TOP}/lib/STM32CubeF4)
 CHECK_LIB_INTEGRITY( ${LIB_DIR__STM32HAL} result)
 if( ${result} STREQUAL FALSE)
-    message("STM32: ${result}")
     FetchContent_Declare(   stm32hal
                             GIT_REPOSITORY https://github.com/STMicroelectronics/STM32CubeF4.git
                             GIT_TAG        master
                             SOURCE_DIR ${LIB_DIR__STM32HAL})
+    FetchContent_MakeAvailable(stm32hal)
 endif()
 
 
@@ -129,11 +130,6 @@ set( STM32HAL_MISC_DEFINE   -DSTM32F411xE
 add_definitions( ${STM32HAL_MISC_DEFINE})
 aux_source_directory( ${LIB_DIR__STM32HAL}/Drivers/STM32F4xx_HAL_Driver/Src  SRC_STM32)
 
-
-
-if( ${result} STREQUAL FALSE)
-    FetchContent_MakeAvailable(stm32hal)
-endif()
 
 list( APPEND        SRC_LIST                ${SRC_STM32})
                                                                  
