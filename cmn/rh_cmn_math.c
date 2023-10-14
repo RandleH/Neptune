233ab72f913b62c700bb54c7cd0f71df359a2935
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2023-10-13 17:57:50 -0700

    Fix warning

--- cmn/rh_cmn_math.c
+++ cmn/rh_cmn_math.c
@@ -8,19 +8,17 @@
 
 
 u8 inline rh_cmn_math__numOf1_32bits( u32 value){
+
+#if 1
     value = ( value & 0x55555555 ) + ( (value >>1)  & 0x55555555 );
     value = ( value & 0x33333333 ) + ( (value >>2)  & 0x33333333 );
     value = ( value & 0x0f0f0f0f ) + ( (value >>4)  & 0x0f0f0f0f );
     value = ( value & 0x00ff00ff ) + ( (value >>8)  & 0x00ff00ff );
     value = ( value & 0x0000ffff ) + ( (value >>16) & 0x0000ffff );
-/* Alternative Method */
-#if 0
-    value = value - ((value >> 1) & 0x55555555);
-    value = (value & 0x33333333) + ((value >> 2) & 0x33333333);
-    value = value + (value >> 8);
-    value = value + (value >> 16);
-#endif
     return value;
+#else
+    return __builtin_popcount(value);
+#endif
 }
 
 u8 inline rh_cmn_math__numOf0_32bits( u32 value){
@@ -28,11 +26,20 @@ u8 inline rh_cmn_math__numOf0_32bits( u32 value){
 }
 
 u8 inline rh_cmn_math__numOf1_8bits( u8 value){
-    __builtin_popcount(value);
+#if 1
+    value = ( value & 0x55 ) + ( (value >>1)  & 0x55 );
+    value = ( value & 0x33 ) + ( (value >>2)  & 0x33 );
+    value = ( value & 0x0f ) + ( (value >>4)  & 0x0f );
+    return value;
+#else
+    return __builtin_popcount(value);
+#endif
+
+    
 }
 
 u8 inline rh_cmn_math__numOf0_8bits( u8 value){
-    __builtin_popcount(~value);
+    return rh_cmn_math__numOf1_8bits(~value);
 }
 
 
